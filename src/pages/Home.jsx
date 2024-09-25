3b6d16d88f4690fddc4d38e9255dbb8459537244
Theus <matheusgoias@hotmail.com>
2024-09-25 18:52:46 +0200

    services

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,47 +2,66 @@ import Header from "../components/header";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  const serviceImages = [
-    {
-      src: "/public/assistenciais-img.png",
-      to: "/",
-      txt: "Planos assistenciais",
-    },
-    {
-      src: "/public/internacionais-img.png",
-      to: "/",
-      txt: "Sistemas internacionais",
-    },
-    {
-      src: "/public/consultorio-img.png",
-      to: "/",
-      txt: "Clínicas e consultórios",
-    },
-  ];
   return (
     <>
       <Header />
-      <div className="flex flex-col items-start pt-8 pl-5">
-        <h1>Nossos serviços</h1>
-        <div className="flex justify-between w-full pr-5 pt-2">
-          {serviceImages.map((x, index) => (
-            <Link to={x.to} key={index}>
+      <div className="flex flex-col items-start py-8 px-10 lg:px-[10%] lg:py-10 w-full h-screen">
+        <h1 className="lg:text-[25px]">Nossos serviços</h1>
+        <div className="flex justify-between w-full pt-2 lg:pt-8">
+          <Link to="/">
+            <div
+              className={`transition-transform hover:translate-y-[-15px] text-white text-xs bg-[url('/public/assistenciais-img.png')] w-[25vw] h-[10vh] lg:h-[45vh] bg-cover`}
+            >
               <div
-                className={`text-white text-xs bg-[url('${x.src}')] w-[25vw] h-[10vh] bg-cover`}
+                className="w-full h-full flex flex-col justify-end text-left"
+                style={{
+                  background:
+                    "var(--Gradient, linear-gradient(180deg, rgba(42, 87, 122, 0.14) 45.87%, rgba(16, 50, 77, 0.76) 79.64%), rgba(217, 217, 217, 0.00))",
+                  opacity: "90%",
+                }}
               >
-                <div
-                  className="w-full h-full flex flex-col justify-end text-left"
-                  style={{
-                    background:
-                      "var(--Gradient, linear-gradient(180deg, rgba(42, 87, 122, 0.14) 45.87%, rgba(16, 50, 77, 0.76) 79.64%), rgba(217, 217, 217, 0.00))",
-                    opacity: "90%",
-                  }}
-                >
-                  <p className="pl-2 pb-2">{x.txt}</p>
-                </div>
+                <p className="pl-2 pb-2 lg:text-[30px] lg:pb-8 lg:pl-8">
+                  Planos assistenciais
+                </p>
               </div>
-            </Link>
-          ))}
+            </div>
+          </Link>
+          <Link to="/">
+            <div
+              className={`transition-transform hover:translate-y-[-15px] text-white text-xs bg-[url('/public/internacionais-img.png')] w-[25vw] h-[10vh] lg:h-[45vh] bg-cover hover:translateY-[15px]`}
+            >
+              <div
+                className="w-full h-full flex flex-col justify-end text-left"
+                style={{
+                  background:
+                    "var(--Gradient, linear-gradient(180deg, rgba(42, 87, 122, 0.14) 45.87%, rgba(16, 50, 77, 0.76) 79.64%), rgba(217, 217, 217, 0.00))",
+                  opacity: "90%",
+                }}
+              >
+                <p className="pl-2 pb-2 lg:text-[30px] lg:pb-8 lg:pl-8">
+                  Sistemas internacionais
+                </p>
+              </div>
+            </div>
+          </Link>
+          <Link to="/">
+            <div
+              className={`transition-transform hover:translate-y-[-15px] text-white text-xs bg-[url('/public/consultorio-img.png')] w-[25vw] h-[10vh] lg:h-[45vh] bg-cover`}
+            >
+              <div
+                className="w-full h-full flex flex-col justify-end text-left"
+                style={{
+                  background:
+                    "var(--Gradient, linear-gradient(180deg, rgba(42, 87, 122, 0.14) 45.87%, rgba(16, 50, 77, 0.76) 79.64%), rgba(217, 217, 217, 0.00))",
+                  opacity: "90%",
+                }}
+              >
+                <p className="pl-2 pb-2 lg:text-[30px] lg:pb-8 lg:pl-8">
+                  Clínicas e consultórios
+                </p>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
     </>
